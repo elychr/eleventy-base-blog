d02ea765d2dd3feb0e8ead60f4f9c8fc935776e6
PFC-Elyas <elyas@passivefirecompany.co.nz>
2020-11-26 18:21:47 +1300

    Update blogposts.js

--- _data/blogposts.js
+++ _data/blogposts.js
@@ -2,63 +2,63 @@ const fetch = require("node-fetch");
 
 // function to get blogposts
 async function getAllBlogposts() {
-  // max number of records to fetch per query
-  const recordsPerQuery = 100;
+    // max number of records to fetch per query
+    const recordsPerQuery = 100;
 
-  // number of records to skip (start at 0)
-  let recordsToSkip = 0;
+    // number of records to skip (start at 0)
+    let recordsToSkip = 0;
 
-  let makeNewQuery = true;
+    let makeNewQuery = true;
 
-  let blogposts = [];
+    let blogposts = [];
 
-  // make queries until makeNewQuery is set to false
-  while (makeNewQuery) {
-    try {
-      // initiate fetch
-      const data = await fetch('https://api.tpfc.co.nz/blog-posts')
+    // make queries until makeNewQuery is set to false
+    while (makeNewQuery) {
+        try {
+            // initiate fetch
+            const data = await fetch('https://api.tpfc.co.nz/blog-posts')
 
-      // store the JSON response when promise resolves
-      const response = await data.json();
+            // store the JSON response when promise resolves
+            const response = await data.json();
 
-      // handle CMS errors
-      if (response.errors) {
-        let errors = response.errors;
-        errors.map((error) => {
-          console.log(error.message);
-        });
-        throw new Error("Houston... We have a CMS problem");
-      }
+            // handle CMS errors
+            if (response.errors) {
+                let errors = response.errors;
+                errors.map((error) => {
+                    console.log(error.message);
+                });
+                throw new Error("Houston... We have a CMS problem");
+            }
 
-      // update blogpost array with the data from the JSON response
-      blogposts = blogposts.concat(response.data.articles);
+            // update blogpost array with the data from the JSON response
+            blogposts = blogposts.concat(response);
 
-      // prepare for next query
-      recordsToSkip += recordsPerQuery;
+            // prepare for next query
+            recordsToSkip += recordsPerQuery;
 
-      // stop querying if we are getting back less than the records we fetch per query
-      if (response.data.articles.length < recordsPerQuery) {
-        makeNewQuery = false;
-      }
-    } catch (error) {
-      throw new Error(error);
+            // stop querying if we are getting back less than the records we fetch per query
+            if (response.length < recordsPerQuery) {
+                makeNewQuery = false;
+            }
+        } catch (error) {
+            throw new Error(error);
+        }
     }
-  }
 
-  // format blogposts objects
-  const blogpostsFormatted = blogposts.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      slug: item.slug,
-      body: item.content,
-      author: item.author,
-      date: item.published_at       
-    };
-  });
+    // format blogposts objects
+    const blogpostsFormatted = blogposts.map((item) => {
+        return {
+            id: item.id,
+            title: item.Title,
+            slug: item.id,
+            body: item.Content,
+            author: item.author,
+            date: item.PublishedAt
+        };
+    });
 
-  // return formatted blogposts
-  return blogpostsFormatted;
+    // return formatted blogposts
+    return blogpostsFormatted;
 }
 
 // export for 11ty
